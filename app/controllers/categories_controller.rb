145644c195e019dd566599323c88e64d68d1f810
Goory al Hamed <gooryalhamed@hotmail.com>
2017-04-23 21:31:51 +0300

    add CRUD actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,16 @@
 class CategoriesController < ApplicationController
+	def index
+		@categories = Category.all
+	end
 	def new
 		@category = Category.new
 	end
 	def create
-		
+		@category = Category.new(category_params)
+		redirect_to categories_path
+	end
+	private
+	def category_params
+		params.require(:category).permit(:name)
 	end
 end

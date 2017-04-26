a2ce01066a4879251f348c52cbd6d39445ccc03d
Goory al Hamed <gooryalhamed@hotmail.com>
2017-04-26 20:21:57 +0300

    drop users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170423081556) do
+ActiveRecord::Schema.define(version: 20170426172042) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -30,14 +30,6 @@ ActiveRecord::Schema.define(version: 20170423081556) do
     t.text     "ingredients"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
   end
 
 end

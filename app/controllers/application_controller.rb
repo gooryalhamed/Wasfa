3aae0301e00713cc3351adcf74d7cc28adb562f8
Goory al Hamed <gooryalhamed@hotmail.com>
2017-04-26 21:54:02 +0300

    correct layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
-	layout Proc.new { |controller| controller.devise_controller? ? 'admin' : 'application' }
+	layout Proc.new { |controller| 
+		if controller.devise_controller?
+			'admin'
+		end
+	}
 	def home
 	end
 	def about

c629d10d5ceda89c45965198b6531ff85f4f7f52
Goory al Hamed <gooryalhamed@hotmail.com>
2017-05-04 00:09:52 +0300

    add dropdown menu on click

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,21 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
+$(document).ready(function(){
+	AdminList()
+})
+
+function AdminList(){
+	var menu = $('#navbar .dropdown')
+	menu.on('click',function(e){
+		if(document.getElementById("admin-menu").style.display=="none"){
+			document.getElementById("admin-menu").style.display = "block"
+		}
+		else{
+			document.getElementById("admin-menu").style.display = "none"
+		}
+		if(e.target.id != menu.attr('id') && !menu.has(e.target).length) {
+			document.getElementById("admin-menu").style.display = "none"
+		}
+	})
+}

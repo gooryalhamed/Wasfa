d64f6c4afe7d4ce171cc31331737b46857a12281
Goory al Hamed <gooryalhamed@hotmail.com>
2017-05-05 15:30:30 +0300

    styling index and _form

--- app/controllers/admin/food_recipes_controller.rb
+++ app/controllers/admin/food_recipes_controller.rb
@@ -1,6 +1,32 @@
 class Admin::FoodRecipesController < ApplicationController
 	layout 'admin'
+	before_action :find_recipe, only: [:show, :edit, :update, :destroy]
 	def index
-		@food_recipes = FoodRecipe.all
+		@recipes = FoodRecipe.all
+	end
+	def new
+		@recipe = FoodRecipe.new
+	end
+	def create
+		@recipe = FoodRecipe.create(recipe_params)
+		redirect_to admin_recipes_path
+	end
+	def edit
+	end
+	def update
+		if @recipe.update(recipe_params) then
+			redirect_to admin_recipes_path
+		end
+	end
+	def destroy
+		@recipe.destroy
+		redirect_to admin_recipes_path
+	end
+	private
+	def recipe_params
+		params.require(:recipe).permit(:name, :origin, :preparation_time, :descripttion, :methhod, :number_of_persons, :image_url, :category_id, :ingredients)
+	end
+	def find_recipe
+		@recipe = FoodRecipe.find(params[:id])
 	end
 end

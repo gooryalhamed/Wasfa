96ab809dd255fac3db5406d6f66e5549e9e04a0a
Goory al Hamed <gooryalhamed@hotmail.com>
2017-05-06 21:45:01 +0300

    add image field to food_recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170506154106) do
+ActiveRecord::Schema.define(version: 20170506155008) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20170506154106) do
     t.text     "ingredients"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.string   "profile_image_id"
   end
 
   create_table "users", force: :cascade do |t|

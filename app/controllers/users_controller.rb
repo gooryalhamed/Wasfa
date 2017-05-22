cdd14f4da69b2f985c540268ecf00af36e3fe354
Goory al Hamed <gooryalhamed@hotmail.com>
2017-05-22 12:30:27 +0300

    upload profile image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,8 @@ class UsersController < ApplicationController
 		File.open(Rails.root.join('public','uploads',uploaded_io.original_filename), "wb") do |file|
 			file.write(uploaded_io.read)
 		end
-		current_user.updaprofile_img = uploaded_io.original_filename
-		if current_user.update(user_params)
-			redirect_to edit_user_path(current_user)
-			flash[:notice] = "Your information were successfully updated"
-		end
+		current_user.update(:username=>params[:user][:username], :email=>params[:user][:email], :profile_img=>uploaded_io.original_filename)
+		redirect_to edit_user_path(current_user)
 	end
 	private
 	def user_params
